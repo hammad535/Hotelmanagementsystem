146794587454f6cf25a0af6394cde7efd8962a67
hammad535 <hammadabbasi637@gmail.com>
2023-06-07 11:28:19 +0500

    final

---
+++ mern-auth-frontend-master/mern-auth-frontend-master/src/pages/home/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Home.scss";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import loginImg from "../../assets/login.svg";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <section className="container hero">
+        <div className="hero-text">
+          <h2>Authentication</h2>
+          
+          <p>
+            Implementation of  User Regisration, Login, Password Reset, Social Login,
+            User Permissions, Email Notifications etc.
+          </p>
+          <div className="hero-buttons --flex-start">
+            <button className="--btn --btn-danger">
+              <Link to="/register">Register</Link>
+            </button>
+            <button className="--btn --btn-primary">
+              <Link to="/login">Login</Link>
+            </button>
+          </div>
+        </div>
+
+         
+      </section>
+    </div>
+  );
+};
+
+export default Home;
